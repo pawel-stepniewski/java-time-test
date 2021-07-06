ef681f5c5c34eeb9c1389dc34aa5b6fa000dd204
Pawel Stepniewski <pawel@stepniewski.tech>
2021-07-06 22:59:06 +0200

    Init commit
    Init project using spring-boot initailizer
    https://start.spring.io/

---
+++ src/main/java/tech/stepniewski/jtime/JtimeApplication.java
@@ -0,0 +1,13 @@
+package tech.stepniewski.jtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/stepniewski/jtime/JtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.stepniewski.jtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
